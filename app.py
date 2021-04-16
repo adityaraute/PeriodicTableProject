b06cd0acdd99a5cc781472a20993e6e4a0889f79
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2021-04-16 16:15:26 +0530

    Skeleton created

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def home():
+    return render_template("index.html")
+
+@app.route("/elements")
+def elements():
+    return render_template("elements.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quizpage.html")
 
 if __name__ == "__main__":
+  app.debug = True
   app.run()
