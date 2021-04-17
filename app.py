fed9d989d884da44af72f145629ac183782df0fb
Heramb <44063354+Hearmb@users.noreply.github.com>
2021-04-18 01:18:54 +0530

    login-signup working

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import pyrebase
 app = Flask(__name__)
+
+firebaseConfig = {
+    "apiKey": "AIzaSyBb6jM5x75-MDw3149Y0ZnRRCWotFyhEwI",
+    "authDomain": "periodictable-f0687.firebaseapp.com",
+    "projectId": "periodictable-f0687",
+    "databaseURL": "https://periodictable-f0687-default-rtdb.firebaseio.com/",
+    "storageBucket": "periodictable-f0687.appspot.com",
+    "messagingSenderId": "285365556906",
+    "appId": "1:285365556906:web:c030eea0feef18f4cee2d3"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 @app.route("/")
 def home():
@@ -9,9 +22,38 @@ def home():
 def elements():
     return render_template("elements.html")
 
-@app.route("/login")
+@app.route("/login",methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        loginEmail = request.form['loginEmail']
+        loginPassword = request.form['loginPassword']
+
+        print(loginEmail,loginEmail)
+        try:
+            auth.sign_in_with_email_and_password(loginEmail, loginPassword)
+            return render_template('elements.html')
+        except:
+            return render_template('login.html', invalidLogin=True)
+    else:
+        return render_template("login.html",invalidLogin=False)
+
+@app.route("/signup",methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        signUpEmail = request.form['signUpEmail']
+        signUpPassword = request.form['signUpPassword']
+        signUpPasswordConfirmed = request.form['signUpPasswordConfirmed']
+        if signUpPassword!=signUpPasswordConfirmed:
+            return render_template('login.html',invalidLogin=True)
+        try:
+            auth.create_user_with_email_and_password(signUpEmail, signUpPassword)
+            return render_template('elements.html')
+        except Exception as e:
+            print(e)
+            print("Got error while sign up")
+            return render_template('login.html',invalidLogin=True)
+    else:
+        return render_template("login.html",invalidLogin=False)
 
 @app.route("/quiz")
 def quiz():
