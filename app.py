ecd8fc9a30d640e6f052a94cebfb1544ea6391b7
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2021-04-18 14:00:37 +0530

    trial pyrebase

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask, render_template
+import pyrebase
+
+config = {
+    'apiKey': "AIzaSyBb6jM5x75-MDw3149Y0ZnRRCWotFyhEwI",
+    'authDomain': "periodictable-f0687.firebaseapp.com",
+    'databaseURL': "periodictable-f0687.firebaseapp.com",
+    'projectId': "periodictable-f0687",
+    'storageBucket': "periodictable-f0687.appspot.com",
+    'messagingSenderId': "285365556906",
+    'appId': "1:285365556906:web:c030eea0feef18f4cee2d3"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+email = 'hello@byebye.in'
+password = 'password'
+user = auth.create_user_with_email_and_password(email, password)
+
+auth.get_account_info(user['idToken'])
+print(user)
 
 app = Flask(__name__)
 
